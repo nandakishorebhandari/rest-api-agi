fee608b1759829e8ff7ed8d8361c176a4f796aed
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-16 11:51:03 +0200

    edit logging

--- server/middleware/mocked-morgan-middleware.js
+++ server/middleware/mocked-morgan-middleware.js
@@ -1,7 +1,6 @@
 const { infoLog, } = require('../utils/logger');
 
 module.exports = (req, res, next) => {
-  console.log();
   console.log(infoLog('req.body:'));
   console.log(req.body);
   next();

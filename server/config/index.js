3f50d8c3c78aa0d290246dc0e40beaa1ef9cb8d5
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-19 01:37:19 +0200

    :ambulance: hotfix

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 
 const config = {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   expireTime: parseInt(process.env.EXPIRE_TIME),
   secret: process.env.JWT_SECRET,
